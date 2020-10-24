5d6356701557cbcf7255f97b4fb29298fc8c2daa
syfqh <nursyafiqah1045@gmail.com>
2020-10-24 02:41:06 +0000

    commit for wait(NULL)

--- 2.2.c
+++ 2.2.c
@@ -1,7 +1,7 @@
 #include <stdlib.h> /* needed to define exit() */
 #include <unistd.h> /* needed for fork() and getpid() */
 #include <stdio.h>  /* needed for printf() */
-
+#include <sys/wait.h>
 int main(int argc, char **argv){
 	int pid; /*process ID*/
 
@@ -11,6 +11,7 @@ int main(int argc, char **argv){
 		break;
 
 	default:  /*a fork returns pid to the parent*/
+		wait(NULL);
 		printf("I am the parent process: pid=%d, child pid=%d\n", getpid(), pid);
 		break;
 	
