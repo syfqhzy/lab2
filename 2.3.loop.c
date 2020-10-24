48b377757eb3dce7838eba7584b031ec0f66a835
syfqh <nursyafiqah1045@gmail.com>
2020-10-24 03:36:05 +0000

    commit for 2.3.loop

---
+++ 2.3.loop.c
@@ -0,0 +1,22 @@
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/wait.h>
+#include <sys/types.h>
+int main(void){
+	for(int i=1; i<13; i++){
+	pid_t pid = fork();
+
+		if(pid == 0){
+			printf("Child process => PPID=%d, PID=%d\n", getppid(), getpid());
+			exit(0);
+		}
+		else{
+			printf("parent process => PID=%d\n", getpid());
+			printf("Waiting for child processes to finish...\n");
+			wait(NULL);
+			printf("Child process finished.\n");
+		}
+	}
+	return EXIT_SUCCESS;
+}
